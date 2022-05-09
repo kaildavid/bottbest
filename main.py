1be8d9abf1b62b9c1e5ddaea66922cf10df25b4f
Th3-Wizard <103869920+Th3-Wizard@users.noreply.github.com>
2022-05-08 23:13:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'EL_Wizard'
+        tl_admin_user = 'Leosmelitho720'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -640,7 +640,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5113150215:AAEpGNuseAUKZIOXaxNH5ZWljeGl-LJQbJc'
+    bot_token = '5319808314:AAFVEjfpxueBPvHXH1FYurLxnNK6ltexacY'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
